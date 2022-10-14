aa1f5100fb560ef5525b310ea835ffc754658478
Adriano Castro <78217027+DricoCastro@users.noreply.github.com>
2022-10-13 23:34:57 -0300

    updatefirst screen home

--- src/web/views/home/index.tsx
+++ src/web/views/home/index.tsx
@@ -1,5 +1,48 @@
-import { HomeHolder } from "./styles";
+import { PrimaryButton } from "../../components/PrimaryButton";
+import {
+  Blur,
+  CardMiddleTexts,
+  DescriptionCardTop,
+  EventBannerStyle,
+  HolderButton,
+  HomeHolder,
+  SubTitleCardTop,
+  TitleCardTop,
+} from "./styles";
+
+const ImageBackground = "/images/homeBackground.jpg";
 
 export const HomePage = () => {
-  return <HomeHolder>HOME</HomeHolder>;
+  function InProgress() {
+    alert("Em Progresso");
+  }
+  return (
+    <HomeHolder>
+      <EventBannerStyle url={ImageBackground}>
+        <Blur>
+          <CardMiddleTexts>
+            <TitleCardTop>
+              Nós Cuidamos do Seu Jardim <br /> Do Plantio à Manutenção
+            </TitleCardTop>
+            <SubTitleCardTop>
+              Nosso time de profissionais em diversas disciplinas garantem
+              performance e qualidade ao seu projeto.
+            </SubTitleCardTop>
+            <DescriptionCardTop>
+              Que um jardim e muito mais que só plantas e vida e que podemos
+              mudar um ambiente deixá-lo mais agradável (MELHORAR ESTILO OUTBACK
+              CITANDO OS SERVIÇOS PRESTADOS...) Clique no botão e simule agora
+              um orçamento. É rápido, vai levar só um minutinho !
+            </DescriptionCardTop>
+            <HolderButton>
+              <PrimaryButton
+                onClick={InProgress}
+                text="Confira nossos trabalhos"
+              />
+            </HolderButton>
+          </CardMiddleTexts>
+        </Blur>
+      </EventBannerStyle>
+    </HomeHolder>
+  );
 };
